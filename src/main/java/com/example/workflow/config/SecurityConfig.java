7bf067f0bda32b1a68f93efb499cbea0eb2d0d7f
Srijit Saini <srijit.saini@gmail.com>
2025-12-15 17:04:15 -0500

    added JWT auth for APIs

--- src/main/java/com/example/workflow/config/SecurityConfig.java
+++ src/main/java/com/example/workflow/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.workflow.config;
 
+import com.example.workflow.security.jwt.JwtAuthenticationFilter;
 import com.example.workflow.service.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -9,18 +10,21 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtAuthenticationFilter jwtFilter;
     private final CustomUserDetailsService userDetailsService;
 
     @Bean
@@ -32,13 +36,16 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/public/**").permitAll()
+                        .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .httpBasic(withDefaults())  // enables HTTP Basic Auth
+                //.httpBasic(withDefaults())  // enables HTTP Basic Auth
                 //.formLogin(withDefaults()) // enables Form Login
-                .userDetailsService(userDetailsService);
+                .userDetailsService(userDetailsService)
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);;
 
         return http.build();
     }
---
+++ src/main/java/com/example/workflow/dto/AuthRequest.java
@@ -0,0 +1,9 @@
+package com.example.workflow.dto;
+
+import lombok.Data;
+
+@Data
+public class AuthRequest {
+    private String username;
+    private String password;
+}
